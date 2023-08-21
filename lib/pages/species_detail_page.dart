eeeb1ad77b16aad1c636c7a3e427745c9e7ec5c5
Mateus Berardo de Souza Terra <mateus.b.s.terra@gmail.com>
2023-08-21 17:08:12 -0300

    Adjust details Screen

--- lib/pages/species_detail_page.dart
+++ lib/pages/species_detail_page.dart
@@ -1,5 +1,7 @@
 import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fruity/app/components/fruityAppBar.dart';
 import 'package:fruity/domain/entities/species.dart';
 
 class SpeciesDetailPage extends StatelessWidget {
@@ -35,26 +37,46 @@ class SpeciesDetailPage extends StatelessWidget {
       child: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
+            pinned: true,
             title: Text(
               title,
               style: const TextStyle(fontStyle: FontStyle.italic),
             ),
             expandedHeight: 350.0,
+            backgroundColor: Theme.of(context).primaryColor,
             flexibleSpace: FlexibleSpaceBar(
-              background: species.picturesUrl!.isNotEmpty ? Swiper(
-                itemBuilder: (context, index) {
-                  final image = species.picturesUrl![index];
-                  return Image.network(
-                    image,
-                    fit: BoxFit.fill,
-                  );
+              background: ShaderMask(
+                shaderCallback: (bound) {
+                  return LinearGradient(
+                      begin: FractionalOffset.topCenter,
+                      end: FractionalOffset.bottomCenter,
+                      colors: [
+                        Colors.black54.withOpacity(0.80),
+                        Colors.transparent
+                      ],
+                      stops: const [
+                        0.0,
+                        0.22
+                      ]).createShader(bound);
                 },
-                indicatorLayout: PageIndicatorLayout.COLOR,
-                autoplay: true,
-                itemCount: species.picturesUrl!.length,
-                pagination: const SwiperPagination(),
-                control: const SwiperControl(),
-              ) : Image.asset('assets/tree-silhouette.png'),
+                blendMode: BlendMode.srcOver,
+                child: species.picturesUrl!.isNotEmpty
+                    ? Swiper(
+                        itemBuilder: (context, index) {
+                          final image = species.picturesUrl![index];
+                          return Image.network(
+                            image,
+                            fit: BoxFit.fill,
+                          );
+                        },
+                        indicatorLayout: PageIndicatorLayout.COLOR,
+                        autoplay: true,
+                        itemCount: species.picturesUrl!.length,
+                        pagination: const SwiperPagination(),
+                        control: const SwiperControl(),
+                      )
+                    : Image.asset('assets/tree-silhouette.png'),
+              ),
             ),
           ),
           SliverList(
@@ -67,20 +89,6 @@ class SpeciesDetailPage extends StatelessWidget {
             ),
             const Padding(
                 padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
-                child: Text("Sobre",
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w300,
-                        color: Colors.grey))),
-            Padding(
-                padding:
-                    const EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0),
-                child: Text(
-                  species.description!,
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
-                )),
-            const Padding(
-                padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
                 child: Text("Temporada",
                     style: TextStyle(
                         fontSize: 15,
@@ -91,6 +99,20 @@ class SpeciesDetailPage extends StatelessWidget {
                     const EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0),
                 child: Text(
                   "De ${toMonthName(species.seasonStartMonth!)} à ${toMonthName(species.seasonEndMonth!)}",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
+                )),
+            const Padding(
+                padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+                child: Text("Sobre",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.grey))),
+            Padding(
+                padding:
+                    const EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0),
+                child: Text(
+                  species.description! * 1000,
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
                 )),
           ]))
